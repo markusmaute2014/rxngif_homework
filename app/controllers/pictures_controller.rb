56cda8fc7bca11839cc05192e3a68e8143c70b9e
Markus Maute <markusmaute2014@kellogg.northwestern.edu>
2013-11-11 15:59:33 -0600

    Homework 6 completed

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,42 @@
 class PicturesController < ApplicationController
+
+
   def show
+  @picture = Picture.find (params[:id])
   end
+
+  def index
+  @picture_list = Picture.all
+  end
+
+  def new
+  #why don´t I have to write anything here - how come this can goes to new
+  end
+
+  def create
+    p = Picture.new
+    p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+    redirect_to "http://localhost:3000/all_pictures", :notice => "Picture has been added...alriiiggght"
+ #why don´t I have to write anything here - how come this can goes to new
+  end
+
+  def destroy
+    Picture.find(params[:id]).destroy
+    redirect_to "http://localhost:3000/all_pictures", :notice => "Picture has been toasted..I smell smoke"
+  end
+
+  def edit
+  @picture_data = Picture.find(params[:id])
+  end
+
+  def update
+   p = Picture.find(params[:id])
+   p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+     redirect_to "http://localhost:3000/all_pictures", :notice => "Picture has been updated"
+  end
+
 end
